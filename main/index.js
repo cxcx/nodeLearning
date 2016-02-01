f9bab2f880b021f601fd4b6f6c37fc58644081b6
chenxin <cxandmjq@gmail.com>
2016-02-01 15:00:37 +0800

    添加response

--- main/index.js
+++ main/index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var app = express();
 app.get('/register', function (req, res) {
+    res.send('connection established');
     console.log('register');
 });
 app.listen(8080);
